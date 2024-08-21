e309f2ca0f70f2b1944e325822fec7c99c46676f
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-08-21 15:45:18 -0300

    first commit

---
+++ src/config/db.ts
@@ -0,0 +1,7 @@
+import dotenv from 'dotenv'
+import { Sequelize } from 'sequelize'
+dotenv.config()
+
+const db = new Sequelize(process.env.DATABASE_URL!)
+
+export default db
---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import colors from 'colors';
+import express from "express";
+import db from "./config/db";
+import router from "./router";
+
+//conectar a DB
+async function connectDB() {
+  try{
+    await db.authenticate()
+    db.sync()
+    console.log(colors.yellow.bold('Conexion exitosa a la BD'))
+  } catch (error){
+    //console.log(error)
+    console.log( colors.red.bold ('Hubo un error al conetar a la BD'))
+  }
+}
+connectDB()
+
+const server = express()
+
+server.use('/api/products/', router)
+
+
+export default server
