f7c9c6ed3a81f176ed1b8c38349a8b4206769da2
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-08-25 20:39:09 -0300

    Actualizando proyecto

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,16 @@
 import { Router } from "express"
-import { body } from 'express-validator'
+import { body, param } from 'express-validator'
 import { createProduct, getProductById, getProducts } from "./handlers/product"
 import { handleInputErrors } from "./middleware"
 
 const router = Router()
 //Routing
 router.get('/', getProducts)
-router.get('/:id', getProductById)
+router.get('/:id',
+  param('id').isInt().withMessage('ID no valido'),
+  handleInputErrors,
+  getProductById)
+
 
 
 router.post('/',
