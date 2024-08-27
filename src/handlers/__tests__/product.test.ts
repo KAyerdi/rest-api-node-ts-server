f1db009825817d91e18e976b68ce397a36616921
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-08-27 19:40:28 -0300

    Actualizando proyecto

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -54,3 +54,49 @@ describe('POST /api/products', () => {
     expect(response.body).not.toHaveProperty('errors')
   })
 })
+
+describe('GET /api/products', () => {
+  test('should check if api/products url exists', async () => {
+    const response = await request(server).get('/api/products')
+    expect(response.status).not.toBe(404)
+  })
+
+  test('GET a JSON response with products', async () => {
+    const response = await request(server).get('/api/products')
+    expect(response.status).toBe(200)
+    expect(response.headers['content-type']).toMatch(/json/)
+    expect(response.body).toHaveProperty('data')
+    expect(response.body.data).toHaveLength(1)
+    expect(response.body).not.toHaveProperty('errors')
+  })
+})
+
+describe('GET /api/products/:id', () => {
+  test('Should return a 404 response for a non-existent product', async () => {
+    const productID = 2000
+    const response = await request(server).get(`/api/products/${productID}`)
+    expect(response.status).toBe(404)
+    expect(response.body).toHaveProperty('error')
+    expect(response.body.error).toBe('Producto No Encontrado')
+  })
+
+  test('Should check a valid ID in the URL', async () => {
+    const response = await request(server).get('/api/products/not-valid-url')
+    expect(response.status).toBe(400)
+    expect(response.body).toHaveProperty('errors')
+    expect(response.body.errors).toHaveLength(1)
+    expect(response.body.errors[0].msg).toBe('ID no valido')
+  })
+
+  test('GET a JSON response for a single product', async () => {
+    const response = await request(server).get('/api/products/1')
+    expect(response.status).toBe(200)
+    expect(response.body).toHaveProperty('data')
+  })
+})
+
+describe('PUT /api/products/:id', () => {
+  test('Should display validation error messages when updating a product', async () => {
+    const response = await request(server).put('/api/products/1').send({})
+  })
+})
