6f881db109f779622aaba935ecc12b2f43a0ced4
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-09-02 20:58:54 -0300

    dejando finalizado el proyecto

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -1,5 +1,6 @@
 import swaggerJSDoc from 'swagger-jsdoc'
 
+
 const options : swaggerJSDoc.Options = {
   swaggerDefinition: {
     openapi:'3.0.2',
@@ -18,4 +19,6 @@ const options : swaggerJSDoc.Options = {
   apis: ['./src/router.ts']
 }
 const swaggerSpec = swaggerJSDoc(options)
+
+
 export default swaggerSpec
