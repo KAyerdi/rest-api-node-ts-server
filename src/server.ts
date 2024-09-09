2f7e529d17cc6a5e2386ec8ce4251271d4389662
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-09-08 22:51:52 -0300

    Actualizando proyecto server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import colors from 'colors';
+import cors, { CorsOptions } from 'cors';
 import express from "express";
+import morgan from 'morgan';
+import swaggerUI from 'swagger-ui-express';
 import db from "./config/db";
-import router from "./router";
-import swaggerUI from 'swagger-ui-express'
 import swaggerSpec from './config/swagger';
+import router from "./router";
 
 //conectar a DB
 export async function connectDB() {
@@ -22,8 +24,22 @@ connectDB()
 //Instancia de express
 const server = express()
 
+//Permitir conexiones
+const corsOptions : CorsOptions = {
+  origin: function(origin, callback){
+    if(origin === process.env.FRONTEND_URL) {
+      callback(null, true)
+    } else {
+      callback(new Error('Error de CORS'))
+    }
+  }
+}
+server.use(cors(corsOptions))
+
 //Leer datos de formularios
 server.use(express.json())
+
+server.use(morgan('dev'))
 
 server.use('/api/products/', router)
 
