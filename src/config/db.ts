2560aee562999946ebf060a9ee8927dfe28c6d59
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-09-16 17:05:08 -0300

    actualizacion

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,8 +3,13 @@ import { Sequelize } from 'sequelize-typescript'
 dotenv.config()
 
 const db = new Sequelize(process.env.DATABASE_URL!,{
-  models: [__dirname + '/../models/**/*.ts'],
-  logging: false
+  models: [__dirname + '/../models/**/*'],
+  logging: false,
+  dialectOptions:{
+    ssl: {
+      require: 'true'
+    }
+  }
 })
 
 export default db
